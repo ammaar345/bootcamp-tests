cb87d4743de53cf01715e4d841a354ffa949c75a
Ammaar <fakirammaar8@gmail.com>
2020-03-22 14:25:54 +0200

    added one more test for final function

--- test/mostProfitableDepartment.test.js
+++ test/mostProfitableDepartment.test.js
@@ -21,6 +21,25 @@ describe("mostProfitableDepartment", function () {
             {department: 'carpentry', sales: 16109, day: 'Friday'}];
         assert.deepEqual(mostProfitableDepartment(salesData), "outdoor");
     });
+    it("The most profitable department is the electronics department",function(){
+    var salesData = [
+        {department: 'hardware', sales: 4500, day: 'Monday'},
+        {department: 'electronics', sales: 1500, day: 'Monday'},
+        {department: 'carpentry', sales: 5500, day: 'Monday'},
+        {department: 'hardware', sales: 7500, day: 'Tuesday'},
+        {department: 'electronics', sales: 2505, day: 'Tuesday'},
+        {department: 'carpentry', sales: 1540, day: 'Tuesday'},
+        {department: 'hardware', sales: 1500, day: 'Wednesday'},
+        {department: 'electronics', sales: 8507, day: 'Wednesday'},
+        {department: 'carpentry', sales: 8009, day: 'Wednesday'},
+        {department: 'hardware', sales: 12000, day: 'Thursday'},
+        {department: 'electronics', sales: 18007, day: 'Thursday'},
+        {department: 'carpentry', sales: 6109, day: 'Thursday'},
+        {department: 'hardware', sales: 7005, day: 'Friday'},
+        {department: 'electronics', sales: 12006, day: 'Friday'},
+        {department: 'carpentry', sales: 16109, day: 'Friday'}];
+    assert.deepEqual(mostProfitableDepartment(salesData), "electronics");
+});
 
 });
 
